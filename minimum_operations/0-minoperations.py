585fa338e94f84a54d512afb31dfa61c39a676d6
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-05-23 09:09:25 +0000

    feat: return minimum operation to copy past n

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that calculates the minimum number of operations to achieve n H characters
+"""
+
+
+def minOperations(n):
+    """
+    Calculate the fewest number of Copy All and Paste operations
+    needed to reach exactly n 'H' characters starting from one 'H'.
+
+    If n is less than or equal to 1, returns 0 since no operations are needed or
+    it's impossible to build up to n > 1 from one character without operations.
+    """
+    if n <= 1:
+        return 0
+
+    ops = 0
+    divisor = 2
+    while divisor * divisor <= n:
+        while n % divisor == 0:
+            ops += divisor
+            n //= divisor
+        divisor += 1
+    if n > 1:
+        ops += n
+
+    return ops
