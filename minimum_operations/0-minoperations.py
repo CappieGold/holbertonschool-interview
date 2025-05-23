090dca959c260e65a48c8e6c97c4b4675914019b
CappieGold <144028326+CappieGold@users.noreply.github.com>
2025-05-23 09:19:46 +0000

    fix: pycodestyle

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module that calculates the minimum number of operations to achieve n H characters
+Module that calculates the minimum number of operations
+to achieve n H characters
 """
 
 
@@ -9,7 +10,8 @@ def minOperations(n):
     Calculate the fewest number of Copy All and Paste operations
     needed to reach exactly n 'H' characters starting from one 'H'.
 
-    If n is less than or equal to 1, returns 0 since no operations are needed or
+    If n is less than or equal to 1,
+    returns 0 since no operations are needed or
     it's impossible to build up to n > 1 from one character without operations.
     """
     if n <= 1:
